fd66f6dac7b7bc0259f38ee9105cf8d1da252c4b
Bryan Parthum <parthum.brayn@epa.gov>
2021-04-09 17:14:10 -0400

    Update clean_sc_ghgs.R

--- sc_ghgs/code/clean_sc_ghgs.R
+++ sc_ghgs/code/clean_sc_ghgs.R
@@ -26,33 +26,33 @@ lapply(packages, pkgTest)
 ##########################
 
 ## Set working directory
-setwd(here())
+setwd(paste0(here(),'\\sc_ghgs'))
 
 ## Inflation from BEA deflator, annual values 113.626/92.486 from: https://apps.bea.gov/iTable/iTable.cfm?reqid=19&step=2#reqid=19&step=2&isuri=1&1921=survey 
 inflation <-  1.228575
 
 ## List of gases
-gas_list <- c('CO2','N2O','CH4')
+gas_list <- c('co2','n2o','ch4')
 
 ##########################
 ##############  START LOOP
 ##########################
 
 for (thisgas in gas_list) {
-  
-  dice_dir    <- paste0("data\\",thisgas,"\\dice")                              # location of file group
-  dice_files  <- fs::dir_ls(dice_dir, regexp = "\\.csv$")                       # create list of .csv files
+
+  dice_dir    <- list.dirs(path=paste0(getwd(),'/data/',thisgas,'/dice'),full.names = TRUE, recursive = TRUE) %>% str_subset(., paste0('/SC-',toupper(thisgas)))
+  dice_files  <- fs::dir_ls(dice_dir, regexp = "\\.csv$")                       
   dice        <- dice_files %>% 
-                     map_dfr(read_csv, .id = "source") %>%                      # read in files (map), turn into data frame (df), and row bind (r)
+                     map_dfr(read_csv, .id = "source") %>%                      
                      as.data.table()
 
-  fund_dir    <- paste0("data\\",thisgas,"\\fund") 
+  fund_dir    <- list.dirs(path=paste0(getwd(),'/data/',thisgas,'/fund'),full.names = TRUE, recursive = TRUE) %>% str_subset(., paste0('/SC-',toupper(thisgas)))
   fund_files  <- fs::dir_ls(fund_dir, regexp = "\\.csv$") 
   fund        <- fund_files %>% 
                      map_dfr(read_csv, .id = "source") %>% 
                      as.data.table()
   
-  page_dir    <- paste0("data\\",thisgas,"\\page") 
+  page_dir    <- list.dirs(path=paste0(getwd(),'/data/',thisgas,'/page'),full.names = TRUE, recursive = TRUE) %>% str_subset(., paste0('/SC-',toupper(thisgas)))
   page_files  <- fs::dir_ls(page_dir, regexp = "\\.csv$") 
   page        <- page_files %>% 
                      map_dfr(read_csv, .id = "source") %>% 
@@ -62,65 +62,41 @@ for (thisgas in gas_list) {
 ###################  CLEAN
 ##########################
 
-dice %<>% mutate(source = str_remove(source,paste0("data/",thisgas,"/dice/"))) %>%
-          mutate(source = str_remove(source,".csv")) %>%
-          separate(source, c("scenario","discount_rate","geography"), " ") %>%
-          mutate(geography = case_when(is.na(geography) ~ 'Global',
-                                       !is.na(geography) ~ 'Domestic'),
-                 scenario = case_when(scenario=="USG1" ~ "IMAGE",
-                                      scenario=="USG2" ~ "MERGE Optimistic",
-                                      scenario=="USG3" ~ "MESSAGE",
-                                      scenario=="USG4" ~ "MiniCAM Base",
-                                      scenario=="USG5" ~ "5th Scenario"),
-                 discount_rate = paste0(as.numeric(discount_rate)*100,'%')) %>%
-          group_by(scenario,discount_rate,geography) %>%
-          mutate(trial = seq(n()))
+clean_mods <- function(x) {
+x %>% mutate(source = gsub(paste0('.*/SC-',toupper(thisgas),'/'),"",source)) %>%
+      mutate(source = str_remove(source,".csv")) %>%
+      separate(source, c("scenario","discount_rate","geography"), " ") %>%
+      mutate(geography = case_when(is.na(geography) ~ 'Global',
+                                    !is.na(geography) ~ 'Domestic'),
+             scenario = case_when(scenario=="USG1" ~ "IMAGE",
+                                  scenario=="USG2" ~ "MERGE Optimistic",
+                                  scenario=="USG3" ~ "MESSAGE",
+                                  scenario=="USG4" ~ "MiniCAM Base",
+                                  scenario=="USG5" ~ "5th Scenario"),
+             discount_rate = paste0(as.numeric(discount_rate)*100,'%')) %>%
+      group_by(scenario,discount_rate,geography) %>%
+      mutate(trial = seq(n()))}
 
-fund %<>% mutate(source = str_remove(source,paste0("data/",thisgas,"/fund/"))) %>%
-          mutate(source = str_remove(source,".csv")) %>%
-          separate(source, c("scenario","discount_rate","geography"), " ") %>%
-          mutate(geography = case_when(is.na(geography) ~ 'Global',
-                                       !is.na(geography) ~ 'Domestic'),
-                 scenario = case_when(scenario=="USG1" ~ "IMAGE",
-                                      scenario=="USG2" ~ "MERGE Optimistic",
-                                      scenario=="USG3" ~ "MESSAGE",
-                                      scenario=="USG4" ~ "MiniCAM Base",
-                                      scenario=="USG5" ~ "5th Scenario"),
-                 discount_rate = paste0(as.numeric(discount_rate)*100,'%')) %>%
-          group_by(scenario,discount_rate,geography) %>%
-          mutate(trial = seq(n()))
-
-page %<>% mutate(source = str_remove(source,paste0("data/",thisgas,"/page/"))) %>%
-          mutate(source = str_remove(source,".csv")) %>%
-          separate(source, c("scenario","discount_rate","geography"), " ") %>%
-          mutate(geography = case_when(is.na(geography) ~ 'Global',
-                                       !is.na(geography) ~ 'Domestic'),
-                 scenario = case_when(scenario=="USG1" ~ "IMAGE",
-                                      scenario=="USG2" ~ "MERGE Optimistic",
-                                      scenario=="USG3" ~ "MESSAGE",
-                                      scenario=="USG4" ~ "MiniCAM Base",
-                                      scenario=="USG5" ~ "5th Scenario"),
-                 discount_rate = paste0(as.numeric(discount_rate)*100,'%')) %>%
-          group_by(scenario,discount_rate,geography) %>%
-          mutate(trial = seq(n()))
+## clean
+dice %<>% clean_mods()
+fund %<>% clean_mods()
+page %<>% clean_mods()
 
 ## WIDE TO LONG
 years <-  paste(seq(2020,2060,5), sep=", ") 
-dice %<>% gather(year,estimate,all_of(years)) %>%
-          mutate(estimate = estimate * inflation, 
-                 model = 'DICE 2010')
-fund %<>% gather(year,estimate,years) %>%
-          mutate(estimate = estimate * inflation, 
-                 model = 'FUND 3.8')
-page %<>% gather(year,estimate,years) %>%
-          mutate(estimate = estimate * inflation, 
-                 model = 'PAGE 2009') %>%
+wide_to_long <- function(x) {
+  x %>% gather(year,estimate,all_of(years)) %>%
+        mutate(estimate = estimate * inflation)}
+
+dice %<>% wide_to_long() %>% mutate(model = 'DICE 2010')
+fund %<>% wide_to_long() %>% mutate(model = 'FUND 3.8')
+page %<>% wide_to_long() %>% mutate(model = 'PAGE 2009') %>%
           as.data.table() %>% 
           setkey(scenario,discount_rate,trial,year,model)
 
 ## load page discontinuities and drop trues
-page_disc <- read_csv(paste0("data\\",thisgas,"_page_discontinuity.csv")) %>% 
-             as.data.table() %>% 
+page_disc <- read_csv(paste0(getwd(),"\\data\\",thisgas,"\\page\\",thisgas,"_discontinuity.csv")) %>% 
+             as.data.table() %>% select(!gas) %>%
              setkey(scenario,discount_rate,trial,year,model) %>%
              mutate(year = as.character(year))
 
@@ -130,7 +106,7 @@ page <- page_disc[page] %>%
                                     discontinuity=='FALSE' ~ estimate)) %>%
         select(!discontinuity)
 
-## COMBINE
+## combine
 data <- rbind(dice,fund,page) %>%
         mutate(gas=paste0(thisgas))
 
@@ -138,56 +114,37 @@ data <- rbind(dice,fund,page) %>%
 ###################  MEANS
 ##########################
 
+## functions
+mod_means <- function(x) {
+  x %>% group_by(geography,year,discount_rate) %>% 
+    dplyr::summarize(mean = mean(estimate, na.rm=TRUE),se   = sd(estimate, na.rm=TRUE)/sqrt(10000))}
+
+high_imp <- function(x) {
+  x %>% filter(discount_rate=='3%') %>%
+    group_by(geography,year) %>%
+    dplyr::summarize(mean = quantile(estimate, 0.95, na.rm=TRUE)) %>%
+    mutate(discount_rate = '95th Pct at 3.0%')}
+
 ## All models
-means <- data %>% group_by(geography,year,discount_rate) %>% dplyr::summarize(mean = mean(estimate, na.rm=TRUE),
-                                                                              se   = sd(estimate, na.rm=TRUE)/sqrt(10000))
-
-## HIGH-IMPACT 95th percentile of each model
-high_impact <- data %>% 
-  filter(discount_rate=='3%') %>%
-  group_by(geography,year) %>%
-  dplyr::summarize(mean = quantile(estimate, 0.95, na.rm=TRUE)) %>%
-  mutate(discount_rate = 'High Impact (95th Pct at 3.0%)')
-
+means <- mod_means(data)
+high_impact <- high_imp(data)
 table <- rbind(means,high_impact)  
 
 
 ## DICE
-dice_means <- dice %>% group_by(geography,year,discount_rate) %>% dplyr::summarize(mean = mean(estimate, na.rm=TRUE),
-                                                                                   se   = sd(estimate, na.rm=TRUE)/sqrt(10000))
-dice_high_impact <- dice %>% 
-  filter(discount_rate=='3%') %>%
-  group_by(geography,year) %>%
-  dplyr::summarize(mean = quantile(estimate, 0.95, na.rm=TRUE)) %>%
-  mutate(discount_rate = 'High Impact (95th Pct at 3.0%)')
-
-dice_table <- rbind(dice_means,dice_high_impact) 
-
+means <- mod_means(dice)
+high_impact <- high_imp(dice)
+dice_table <- rbind(means,high_impact) 
 
 ## FUND
-fund_means <- fund %>% group_by(geography,year,discount_rate) %>% dplyr::summarize(mean = mean(estimate, na.rm=TRUE),
-                                                                                   se   = sd(estimate, na.rm=TRUE)/sqrt(10000))
-
-fund_high_impact <- fund %>% 
-  filter(discount_rate=='3%') %>%
-  group_by(geography,year) %>%
-  dplyr::summarize(mean = quantile(estimate, 0.95, na.rm=TRUE)) %>%
-  mutate(discount_rate = 'High Impact (95th Pct at 3.0%)')
-
-fund_table <- rbind(fund_means,fund_high_impact) 
-
+means <- mod_means(fund)
+high_impact <- high_imp(fund)
+fund_table <- rbind(means,high_impact) 
 
 ## PAGE
-page_means <- page %>% group_by(geography,year,discount_rate) %>% dplyr::summarize(mean = round(mean(estimate, na.rm=TRUE),0),
-                                                                                   se   = round(sd(estimate, na.rm=TRUE),0)/sqrt(10000))
-
-page_high_impact <- page %>% 
-  filter(discount_rate=='3%') %>%
-  group_by(geography,year) %>%
-  dplyr::summarize(mean = quantile(estimate, 0.95, na.rm=TRUE)) %>%
-  mutate(discount_rate = 'High Impact (95th Pct at 3.0%)')
-
-page_table <- rbind(page_means,page_high_impact) 
+means <- mod_means(page)
+high_impact <- high_imp(page)
+page_table <- rbind(means,high_impact) 
 
 ##########################
 ####################  SAVE
