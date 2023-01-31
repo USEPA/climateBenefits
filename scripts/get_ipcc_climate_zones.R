05d0a36bf326b9016a0db512c29ef9fdee8097de
bryanparthum <parthum.bryan@epa.gov>
2023-01-31 15:32:56 -0500

    add climate zones for ches wb

---
+++ scripts/get_ipcc_climate_zones.R
@@ -0,0 +1,180 @@
+##########################
+#################  library
+##########################
+
+## clear workspace
+rm(list = ls())
+gc()
+
+
+## this function will check if a package is installed, and if not, install it
+list.of.packages <- c('magrittr','tidyverse',
+                      'stringr',
+                      'sf','USAboundaries',
+                      'ggplot2','ggpubr','scales','showtext', 'ggsn', 'cowplot')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+lapply(list.of.packages, library, character.only = TRUE)
+
+##########################
+##################### data
+##########################
+
+## watershed boundary
+watershed =
+  st_read('maps/chesapeakeBayWatershed/Chesapeake_Bay_Watershed_Boundary.shp') %>%
+  mutate(aes = 'Chesapeake Bay\nWatershed')
+
+# ## test plot
+# watershed %>%
+#   ggplot() +
+#   geom_sf() + 
+#   theme_void()
+
+## get climate zones from ipcc
+zones = 
+  st_read('store/climateMap/ipcc_zones_2017_names.shp')
+
+# ## test plot
+# zones %>%
+#   ggplot() +
+#   geom_sf() +
+#   theme_void()
+
+## get lakes from study area
+lakes = 
+  st_read('store/study_lakes/study_lakes.shp') %>% 
+  select(WB_ID, AREASQK) %>% 
+  st_transform(st_crs(zones))
+
+# ## test plot
+# lakes %>%
+#   ggplot() +
+#   geom_sf() +
+#   theme_void()
+
+## get centroids for climate zone intersection
+lake.points = 
+  lakes %>% 
+  st_centroid
+
+## join waterbody points with climate zones to get climate zone info
+data = 
+  left_join(
+    lakes,
+    st_join(lake.points, 
+            zones %>% 
+              st_make_valid, 
+            join = st_within) %>% 
+      st_drop_geometry %>% 
+      as_tibble %>% 
+      select(WB_ID, clmt_zn) %>% 
+      rename(climate.zone = clmt_zn),
+    by = 'WB_ID'
+  )
+
+## clean house
+rm(lake.points)
+gc()
+
+## classify as pond or reservoir
+data %<>% 
+  mutate(type = case_when(AREASQK <  0.08 ~ 'pond',
+                          AREASQK >= 0.08 ~ 'reservoir'))
+## check
+data %>% st_drop_geometry %>% select(type) %>% table
+
+## rename to title case
+data %<>% 
+  mutate(climate.zone = str_to_title(climate.zone))
+
+## export waterbody type
+data %>% 
+  st_drop_geometry %>% 
+  select(WB_ID, climate.zone, type) %>% 
+  write_csv('store/climate_zones_for_ches_waterbodies.csv')
+
+## get shoreline
+shoreline =
+  st_read('maps/Chesapeake_Bay_Shoreline_Medium_Resolution.shp')
+
+# ## test plot
+# shoreline %>%
+#   ggplot() +
+#   geom_sf() + 
+#   theme_void()
+
+## get state polygons
+states =
+  st_as_sf(USAboundaries::us_states() %>%
+             filter(name %in% c("Virginia", "Maryland", "Delaware",
+                                "West Virginia", "Pennsylvania", "New York")) %>%
+             st_transform(crs = st_crs(watershed)))
+
+us_states = subset(USAboundaries::us_states(),
+                   !name %in% c("United States Virgin Islands",
+                                "Commonwealth of the Northern Mariana Islands",
+                                "Guam",
+                                "American Samoa",
+                                "Puerto Rico",
+                                "Alaska",
+                                "Hawaii")) %>%
+  st_transform(crs = st_crs(5070))
+
+## region box
+region_box =
+  st_as_sfc(st_bbox(states)) %>% 
+  st_transform(crs = st_crs(watershed))
+
+## test plot
+region = 
+  ggplot() + 
+  geom_sf(data = us_states, fill = "white", size = 0.2) + 
+  geom_sf(data = region_box, fill=NA, color='#9DBF9E', size=1.2) +
+  theme_void()
+
+##########################
+##################### plot
+##########################
+
+study_area = 
+  ggplot() +
+  geom_sf(data  = states,
+          color = 'black',
+          alpha = 0.05) + 
+  geom_sf(data  = watershed,
+          aes(color = aes,
+              fill  = aes),
+          alpha = 0.2) + 
+  geom_sf(data  = shoreline,
+          fill  = 'white') + 
+  geom_sf(data  = data,
+          aes(color  = climate.zone,
+              fill  = climate.zone)) + 
+  geom_sf_text(data = states, 
+               aes(label = stusps)) +
+  scale_color_manual(values = c("#9DBF9E", "#56B4E9", "#E69F00")) +
+  scale_fill_manual(values = c("#9DBF9E","#56B4E9", "#E69F00")) +
+  north(data     = states,
+        location = "topright") + 
+  scalebar(data      = states,
+           location  = "bottomright",
+           transform = T,
+           dist      = 50,
+           dist_unit = "mi") +
+  labs(color = '',
+       fill  = '',
+       label = '') +
+  theme_void() +
+  theme(legend.position = c(0.89, 0.3),
+        legend.key = element_rect(color=NA))
+
+## draw study area inset 
+ggdraw() +
+  draw_plot(study_area) +
+  draw_plot(region, x = 0.05, y = 0.55, width = 0.35, height = 0.35)
+
+## export
+ggsave("output/figures/climate_zones.png", width  = 6, height = 8)
+
+## end of script. have a great day! 
