092d2f8c91fbea1baf2a87f6e36cc1367ad79b0e
bryanparthum <parthum.bryan@epa.gov>
2023-02-03 16:41:00 -0500

    add emissions reductions in mrb

--- scripts/get_reductions_in_mrb.R
+++ scripts/get_reductions_in_mrb.R
@@ -8,111 +8,144 @@ gc()
 
 ## this function will check if a package is installed, and if not, install it
 list.of.packages <- c('magrittr','tidyverse',
-                      'stringr',
-                      'sf')
+                      'sf',
+                      'arrow')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
 lapply(list.of.packages, library, character.only = TRUE)
 
 ##########################
-##################### data
+#################### parts
 ##########################
 
-# ## get emissions by climate zone and type from main markdown
-# emissions.by.type.ch4 = read_csv('output/emissions_by_lake_characteristics_ch4.csv')
-# emissions.by.type.co2 = read_csv('output/emissions_by_lake_characteristics_co2.csv')
-# emissions.by.type.n2o = read_csv('output/emissions_by_lake_characteristics_n2o.csv')
-
-# ## get state polygons
-# us_states = subset(USAboundaries::us_states(),
-#                    !name %in% c("United States Virgin Islands",
-#                                 "Commonwealth of the Northern Mariana Islands",
-#                                 "Guam",
-#                                 "American Samoa",
-#                                 "Puerto Rico",
-#                                 "Alaska",
-#                                 "Hawaii")) %>%
-#   st_transform(crs = st_crs(5070))
-
-## get table that we want
-## start clock
-start.read.time <- proc.time()
-national = 
-  st_read('../climateBenefits.gpkg', 'national') %>%
-  st_set_crs(4269)
-## stop clock
-read.time = proc.time() - start.read.time
-
-## check read time, 700 seconds
-print('time it took to read ghg database: ')
-read.time
-
-## check distinct types of waterbodies
-national %>% st_drop_geometry %>% dplyr::select(subtype) %>% table
-# subtype
-# canals and ditches    freshwater pond     reservoir 
-# 272058                6676699             126181 
-
-## keep only ponds and reservoirs
-## start clock
-start.filter.time <- proc.time()
-national %<>% filter(subtype %in% c('freshwater pond', 'reservoir'))
-## stop clock
-filter.time = proc.time() - start.filter.time
-
-## check read time, 274 seconds
-print('time it took to filter to ponds and reservoirs: ')
-filter.time
+## function to clean give data
+read_mrb_waterbodies = function(x) {
+  read_parquet(x, show_col_types = F)
+}
 
 ## watershed boundary
 watershed =
   st_read('store/Miss_RiverBasin/Miss_RiverBasin.shp')
 
-## get centroids for climate zone intersection
-national.points =
-  national %>%
-  st_centroid %>% 
-  st_transform(st_crs(watershed))
+## get state polygons
+us.states = subset(USAboundaries::us_states(),
+                   !name %in% c("United States Virgin Islands",
+                                "Commonwealth of the Northern Mariana Islands",
+                                "Guam",
+                                "American Samoa",
+                                "Puerto Rico",
+                                "Alaska",
+                                "Hawaii")) %>%
+  st_transform(crs = st_crs(watershed))
 
-## test projection on a subset
-# t = 
-#   national.points[1:1000,] %>%
-#   st_set_crs(4269) %>% 
-#   st_transform(st_crs(watershed))
-# 
-# ## plot to check projections
-# ggplot() +
-#   geom_sf(data  = us_states,
-#           color = 'black',
-#           alpha = 0.05) +
-#   geom_sf(data  = watershed,
-#           color = "#56B4E9",
-#           fill  = "#56B4E9",
-#           alpha = 0.1) +
-#   geom_sf(data  = t,
-#           aes(color = climate,
-#               fill  = climate))
+## get states in watershed
+mrb.states = 
+  us.states %>% 
+  st_filter(watershed) %>% 
+  st_drop_geometry %>% 
+  .$state_abbr
 
-## clean house
-rm(national)
+##########################
+##################### data
+##########################
+
+## read waterbodies from ghgrp within the mississippi river basin (from get_ghg_waterbodies_in_mrb.R)
+mrb.waterbodies = 
+  list.files('output/ghg_in_mrb/', pattern = "*.parquet", full.names = T) %>%
+  map_df(~read_mrb_waterbodies(.)) %>% 
+  .$globalid
+
+## read all waterbodies from ghgrp, filter to those in mississippi river basin
+## TO-DO test if SQL query will work for this filer to speed up the read_sf, query is available as an option in read_sf: query = "SELECT * FROM national WHERE globalid in mrb.waterbodies;"
+data = 
+  read_sf('../climateBenefits.gpkg') %>%
+  dplyr::select(globalid, stusps, climate, subtype, area_ha_new, co2.tonnes.ha.y.2021, ch4.tonnes.ha.y.2021) %>% 
+  filter(globalid %in% mrb.waterbodies)
+
+## get emissions by climate zone and type from main markdown
+emissions.by.type = 
+  left_join(read_csv('output/emissions_by_lake_characteristics_ch4.csv', show_col_types = F),
+            read_csv('output/emissions_by_lake_characteristics_co2.csv', show_col_types = F),
+            read_csv('output/emissions_by_lake_characteristics_n2o.csv', show_col_types = F),
+            by = c('type', 'climate.zone')) %>% 
+  rename(subtype = type,
+         climate = climate.zone) %>% 
+  mutate(subtype = case_when(subtype == 'pond' ~ 'freshwater pond', T ~ subtype))
+
+## collect garbage
 gc()
 
-## trim waterbodies to watershed
-## start clock
-start.trim.time <- proc.time()
-mrb.points =
-  national.points %>%
-  st_filter(watershed)
-## stop clock
-trim.time = proc.time() - start.trim.time
+## get average emissions rates by subtype and climate to identify closest subtypes and climates
+data %>% 
+  st_drop_geometry %>% 
+  group_by(climate, subtype) %>% 
+  summarize(co2.tonnes.ha.y.2021 = mean(co2.tonnes.ha.y.2021, na.rm = T),
+            ch4.tonnes.ha.y.2021 = mean(ch4.tonnes.ha.y.2021, na.rm = T)) %>% 
+  arrange(subtype, ch4.tonnes.ha.y.2021)
 
-## check computation time
-print('time it took to trim waterbodies to mrb watershed: ')
-trim.time
+# climate              subtype         co2.tonnes.ha.y.2021 ch4.tonnes.ha.y.2021
+# <chr>                <chr>                          <dbl>                <dbl>
+#   1 boreal               freshwater pond                 3.45               0.183
+# 2 cool temperate       freshwater pond                 3.74               0.183
+# 3 tropical dry/montane freshwater pond               NaN                  0.183
+# 4 tropical moist/wet   freshwater pond                10.2                0.183
+# 5 warm temperate dry   freshwater pond                 6.23               0.183
+# 6 warm temperate moist freshwater pond                 5.35               0.183
+# 7 boreal               reservoir                     NaN                  0.0136
+# 8 cool temperate       reservoir                       3.74               0.0540
+# 9 warm temperate moist reservoir                       5.35               0.0804
+# 10 tropical moist/wet   reservoir                     NaN                  0.141
+# 11 warm temperate dry   reservoir                       6.23               0.151
+# 12 tropical dry/montane reservoir                     NaN                  0.284
 
-## export
-mrb.points %>% st_write('output/ghg_waterbodies_in_mrb/ghg_waterbodies_in_mrb_points.shp')
+## from the chesapeake bay we have cool temperate and warm temperate moist. 
+## for freshwater ponds and ch4, there is no variation in emission rates across climate.
+## for reservoirs and ch4, cool temperate gets mapped to boreal, and warm temperate moist for everything else.
+## for freshwater ponds and co2, cool temperate gets mapped to boreal, and warm temperate moist for everything else.
+## for reservoirs and co2, the same is true, although there are only emission rates for three zones. 
 
+## get comparable climate zones for merging emissions reducitons
+data %<>% 
+  mutate(climate.ches.bay = case_when(climate == 'Boreal' ~ 'Cool Temperate',
+                                      T ~ 'Warm Temperate Moist'))
 
+## merge with chesapeake bay emissions reductions
+data %<>% 
+  left_join(emissions.by.type, 
+            by = c('climate.ches.bay' = 'climate', 'subtype'))
+
+## estimate emissions for waterbodies in the mrb
+data %<>% 
+  mutate(co2.emissions = area_ha_new * co2.tonnes.ha.y.2021,
+         ch4.emissions = area_ha_new * (ch4.tonnes.ha.y.2021 + (ch4.tonnes.ha.y.2021 * 0.09)),
+         co2.reduction = co2.emissions * incr.co2.mean.pct,
+         ch4.reduction = ch4.emissions * incr.ch4.mean.pct)
+
+## get total emissions reductions
+data %>% 
+  st_drop_geometry %>% 
+  summarize(co2.reduction = sum(co2.reduction, na.rm = T),
+            ch4.reduction = sum(ch4.reduction, na.rm = T))
+
+## count number of waterbodies with co2 emissions estimates
+data %>% 
+  st_drop_geometry %>% 
+  filter(!is.na(co2.reduction)) %>% 
+  nrow
+
+## export results by state
+for(STATE in mrb.states){
+  data %>%
+    filter(stusps == STATE) %>%
+    st_write(paste0('output/mrb_emissions_reductions/mrb_emissions_reductions_', STATE, '.shp'))
+}
+
+## export results by state as text files
+for(STATE in mrb.states){
+  data %>%
+    st_drop_geometry %>% 
+    filter(stusps == STATE) %>%
+    write_parquet(paste0('output/mrb_emissions_reductions_text/mrb_emissions_reductions_text_', STATE, '.parquet'))
+}
 
 ## end of script. have a great day! 
