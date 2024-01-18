7577f64d8b4fabe98f999e34fd8067b7aac9a7aa
Beaulieu <Beaulieu.Jake@epa.gov>
2024-01-18 16:39:14 -0500

    Added MRB to study map.  Added NDH polygons to MRB extrapolation.  Updated renv.

--- scripts/figures/study_area_map.R
+++ scripts/figures/study_area_map.R
@@ -12,22 +12,14 @@
 rm(list = ls())
 gc()
 
-## This function will check if a package is installed, and if not, install it
-pkgTest <- function(x) {
-  if (!require(x, character.only = T))
-  {
-    install.packages(x, dep = T)
-    if(!require(x, character.only = T)) stop("Package not found")
-  }
-}
-
-packages <- c('data.table','tidyverse','sf','readxl','USAboundaries',
-              'viridis','epiDisplay',
-              'ggmap','ggplot2','ggpubr','cowplot',
-              'rnaturalearth','rnaturalearthdata',
-              'maps','mapview','here','rcartocolor','ggsn','extrafont')
-lapply(packages, pkgTest)
-
+# libraries vis renv controlled library
+library(tidyverse)
+library(sf)
+library(USAboundaries)
+library(ggsn) # north and scale functions
+library(cowplot) # ggdraw
+library(tictoc)
+library(arrow) # read parquet
 ####################################################
 #################################  WORKING DIRECTORY
 ####################################################
@@ -42,12 +34,13 @@ setwd(here())
 ##############################################  DATA
 ####################################################
 
+# Chesapeake Bay
 watershed <- st_read('maps\\chesapeakeBayWatershed\\Chesapeake_Bay_Watershed_Boundary.shp') %>% mutate(aes = 'Chesapeake Bay\nWatershed')
 # watershed %>% 
 #   ggplot() +
 #   geom_sf() + theme_void()
 
-lakes <- st_read('store\\study_lakes\\study_lakes.shp') %>% mutate(aes = 'Waterbodies')
+lakes <- st_read('store\\study_lakes\\study_lakes.gpkg') %>% mutate(aes = 'Waterbodies')
 # lakes %>% 
 #   ggplot() +
 #   geom_sf() + theme_void()
@@ -62,7 +55,59 @@ states <- st_as_sf(us_states() %>%
                                         "West Virginia", "Pennsylvania", "New York")) %>%
                      st_transform(crs = st_crs(watershed)))
 
-us_states = subset(us_states(), 
+##########################
+############## Mississippi
+##########################
+ms_watershed <- st_read('store\\Miss_RiverBasin\\Miss_RiverBasin.shp') %>% 
+  mutate(aes = 'Mississippi/Atchafalaya\nWatershed') %>%
+  st_transform(crs = st_crs(watershed))
+
+tic() # 96 seconds at home.  12sec in office.  read in list of lakes in watershed
+ms_lakes_ <- list.files('output/lakes_in_mrb/', 
+                        pattern = "*.parquet", 
+                        full.names = T) %>%
+     map_df(~read_parquet(.))
+toc()
+nrow(ms_lakes_) # 3,908,549
+head(ms_lakes_)
+unique(ms_lakes_$comid) # 289, including NA
+table(ms_lakes_$comid) # several have many duplicates?  These are polygons broken up by intersection with state or NWI
+
+
+# Read in all US flooded lands
+us_flood_lds <- st_read('store/climateBenefits.gpkg', 'national') %>%
+  mutate(aes = 'Waterbodies') %>%
+  st_transform(crs = st_crs(watershed))
+nrow(us_flood_lds) #7,074,938
+
+
+us_flood_lds %>% st_drop_geometry %>% filter(comid %in% unique(na.omit(ms_lakes_$comid))) %>% nrow #11,618 
+us_flood_lds %>% st_drop_geometry %>% filter(globalid %in% unique(na.omit(ms_lakes_$globalid))) %>% nrow #3,897,585
+
+
+
+tic() # 19.5 seconds. filter out those included in MS watershed
+ms_lakes <- us_flood_lds %>% filter((comid %in% unique(na.omit(ms_lakes_$comid)))| 
+                                      (globalid %in% unique(na.omit(ms_lakes_$globalid))))
+toc()
+nrow(ms_lakes) #3,909,203
+
+# make subset for code development 
+#ms_lakes <- ms_lakes %>% slice_sample(prop = 0.10)
+# ms_lakes %>%
+#   ggplot() +
+#   geom_sf(color = NA, fill = "deepskyblue2") + theme_void()
+
+
+## Get Mississippi River basin states
+ms_states <- us_states() %>% 
+  st_filter(ms_watershed)
+# ms_states %>%
+#   ggplot() +
+#   geom_sf() + theme_void()
+
+## Continernous states for locater map
+contim_states = subset(us_states(),  
                    !name %in% c("United States Virgin Islands",
                                 "Commonwealth of the Northern Mariana Islands",
                                 "Guam",
@@ -76,37 +121,64 @@ us_states = subset(us_states(),
 ########################################  PLOT PARTS
 ####################################################
 
-region_box <- st_as_sfc(st_bbox(states)) %>% st_transform(crs = st_crs(watershed))
-
 region <- ggplot() + 
-  geom_sf(data=us_states, fill = "white", size = 0.2) + 
-  geom_sf(data=region_box, fill=NA, color='#9DBF9E', size=1.2) +
+  geom_sf(data=contim_states, fill = "white", size = 0.2) + 
+  #geom_sf(data=region_box, fill=NA, color='#9DBF9E', size=1.2) +
+  geom_sf(data=watershed, fill = "#9DBF9E", alpha = 0.4) +
+  geom_sf(data = ms_watershed, fill = "#996633", alpha = 0.4) +
+  #ggsn::north(data=contim_states,location="topleft", scale =0.25) + 
   theme_void()
 
 ####################################################
 ##############################################  PLOT
 ####################################################
 
-study_area <- ggplot() +
+cb_study_area <- ggplot() +
   geom_sf(data=states,color='black',alpha=0.05) + 
-  geom_sf(data=watershed,aes(fill=aes),alpha=0.4) + 
+  geom_sf(data=watershed,fill="#9DBF9E",alpha=0.4) + 
   geom_sf(data=shoreline,fill='white') + 
-  geom_sf(data=lakes,color='deepskyblue2',aes(fill=aes)) + 
+  geom_sf(data=lakes,color='deepskyblue2',fill='deepskyblue2') + 
   geom_sf_text(data=states, aes(label=stusps)) +
-  scale_fill_manual(values=c("#9DBF9E",'deepskyblue2')) +
+  #scale_fill_manual(values=c("#9DBF9E",'deepskyblue2')) +
   # scale_color_carto_d(type ="diverging", palette="Earth") +
   theme_void() +
-  north(data=states,location="topright") + 
-  scalebar(data=states,location="bottomright",transform=T,dist=50,dist_unit="mi") +
+  #ggsn::north(data=states,location="topleft", scale =0.25) + 
+  scalebar(data=states,location="bottomright",
+           transform=T,dist=50,dist_unit="mi", st.size = 2) +
   labs(fill='') +
-  theme(legend.position = c(0.89, 0.3),
-        legend.key = element_rect(color=NA))
-  
-plot <- ggdraw() +
-  draw_plot(study_area) +
-  draw_plot(region, x = 0.05, y = 0.55, width = 0.35, height = 0.35)
+  theme(legend.position = c(0.1, 0.9),
+        legend.key = element_rect(color=NA),
+        plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Chesapeake Bay Watershed")
 
-ggsave(filename = "output\\figures\\study_area.png", 
+
+ms_study_area <- ggplot() +
+  geom_sf(data=ms_states,color='black',alpha=0.05) + 
+  geom_sf(data=ms_watershed, fill="#996633", alpha = 0.4) + 
+  geom_sf(data=ms_lakes, color='deepskyblue2', size = 0.1, aes(fill=aes)) + 
+  geom_sf_text(data=ms_states, aes(label=stusps)) +
+  scale_fill_manual(values='deepskyblue2') +
+  # scale_color_carto_d(type ="diverging", palette="Earth") +
+  theme_void() +
+  ggsn::north(data=ms_states,location = "bottomright",
+              scale = 0.2) + 
+  scalebar(data=ms_states,location="bottomleft",
+           transform=T,dist=250,dist_unit="mi", st.size = 2) +
+  labs(fill='') +
+  ggtitle("Mississippi/Atchafalya Watershed") +
+  theme(legend.position = c(0.15, 0.17),
+        legend.key = element_rect(color=NA),
+        plot.title = element_text(hjust = 0.5)) 
+  
+
+# plot <- cowplot::ggdraw() +
+#   draw_plot(cb_study_area, x = 0.5, y = 0.5, width = 0.35, height = 0.35) +
+#   draw_plot(ms_study_area, x=0.25, y=0.01, width = 0.75) +
+#   draw_plot(region, x = 0.05, y = 0.75, width = 0.35, height = 0.35)
+
+top_row <- plot_grid(region, cb_study_area)
+plot <- plot_grid(top_row, ms_study_area, ncol=1)
+ggsave(filename = "output\\figures\\combined_study_area.png", 
        plot = plot,
        width = 6, 
        height = 8,
---
+++ scripts/get_reductions_in_mrb_ch4_globalid.R
@@ -0,0 +1,270 @@
+##########################
+#################  library
+##########################
+
+## clear workspace
+rm(list = ls())
+gc()
+
+## this function will check if a package is installed, and if not, install it
+list.of.packages <- c('magrittr','tidyverse',
+                      'sf',
+                      'arrow')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+lapply(list.of.packages, library, character.only = TRUE)
+
+##########################
+#################### parts
+##########################
+
+## function to read all files in a directory
+read_mrb_waterbodies = function(x) {
+  read_parquet(x)
+}
+
+## watershed boundary
+watershed =
+  st_read('store/Miss_RiverBasin/Miss_RiverBasin.shp')
+
+## get state polygons
+us.states = subset(USAboundaries::us_states(),
+                   !name %in% c("United States Virgin Islands",
+                                "Commonwealth of the Northern Mariana Islands",
+                                "Guam",
+                                "American Samoa",
+                                "Puerto Rico",
+                                "Alaska",
+                                "Hawaii")) %>%
+  st_transform(crs = st_crs(watershed))
+
+## get states in watershed
+mrb.states = 
+  us.states %>% 
+  st_filter(watershed) %>% 
+  st_drop_geometry %>% 
+  .$state_abbr
+
+##########################
+##################### data
+##########################
+
+## read waterbodies from ghgrp within the mississippi river basin (from get_ghg_waterbodies_in_mrb.R)
+mrb.waterbodies =
+  list.files('output/lakes_in_mrb/', pattern = "*.parquet", full.names = T) %>%
+  map_df(~read_mrb_waterbodies(.))
+
+## read all waterbodies from ghgrp, filter to those in mississippi river basin
+## TO-DO test if SQL query will work for this filer to speed up the read_sf, query is available as an option in read_sf: query = "SELECT * FROM national WHERE globalid in mrb.waterbodies;"
+data =
+  read_sf('store/climateBenefits.gpkg') %>%
+  dplyr::select(globalid, comid, stusps, climate, subtype, area_ha_new, co2.tonnes.ha.y.2021, ch4.tonnes.ha.y.2021) %>%
+  filter((comid %in% unique(na.omit(mrb.waterbodies$comid)))| 
+           (globalid %in% unique(na.omit(mrb.waterbodies$globalid))))
+
+
+## get emissions by climate zone and type from main markdown
+emissions.by.type =
+  left_join(
+    left_join(
+      readr::read_csv('output/emissions_by_lake_characteristics_ch4.csv', show_col_types = F),
+      readr::read_csv('output/emissions_by_lake_characteristics_co2.csv', show_col_types = F),
+      by = c('type', 'climate.zone')
+    ),
+    readr::read_csv('output/emissions_by_lake_characteristics_n2o.csv', show_col_types = F),
+    by = c('type', 'climate.zone')
+  ) %>%
+  rename(subtype = type,
+         climate = climate.zone) %>%
+  mutate(subtype = case_when(subtype == 'pond' ~ 'freshwater pond', T ~ subtype))
+
+## collect garbage
+gc()
+
+## get average emissions rates by subtype and climate to identify closest subtypes and climates
+data %>%
+  st_drop_geometry %>%
+  group_by(climate, subtype) %>%
+  summarize(co2.tonnes.ha.y.2021 = mean(co2.tonnes.ha.y.2021, na.rm = T),
+            ch4.tonnes.ha.y.2021 = mean(ch4.tonnes.ha.y.2021, na.rm = T)) %>%
+  arrange(subtype, ch4.tonnes.ha.y.2021)
+
+# climate              subtype         co2.tonnes.ha.y.2021 ch4.tonnes.ha.y.2021
+# <chr>                <chr>                          <dbl>                <dbl>
+#   1 boreal               freshwater pond                 3.45               0.183
+# 2 cool temperate       freshwater pond                 3.74               0.183
+# 3 tropical dry/montane freshwater pond               NaN                  0.183
+# 4 tropical moist/wet   freshwater pond                10.2                0.183
+# 5 warm temperate dry   freshwater pond                 6.23               0.183
+# 6 warm temperate moist freshwater pond                 5.35               0.183
+# 7 boreal               reservoir                     NaN                  0.0136
+# 8 cool temperate       reservoir                       3.74               0.0540
+# 9 warm temperate moist reservoir                       5.35               0.0804
+# 10 tropical moist/wet   reservoir                     NaN                  0.141
+# 11 warm temperate dry   reservoir                       6.23               0.151
+# 12 tropical dry/montane reservoir                     NaN                  0.284
+
+## from the chesapeake bay we have cool temperate and warm temperate moist. 
+## for freshwater ponds and ch4, there is no variation in emission rates across climate.
+## for reservoirs and ch4, cool temperate gets mapped to boreal, and warm temperate moist for everything else.
+## for freshwater ponds and co2, cool temperate gets mapped to boreal, and warm temperate moist for everything else.
+## for reservoirs and co2, the same is true, although there are only emission rates for three zones. 
+
+## get comparable climate zones for merging emissions reductions
+data %<>%
+  mutate(climate.ches.bay = case_when(climate == 'Boreal' ~ 'Cool Temperate',
+                                      T ~ 'Warm Temperate Moist'))
+
+## merge with chesapeake bay emissions reductions
+data %<>%
+  left_join(emissions.by.type,
+            by = c('climate.ches.bay' = 'climate', 'subtype'))
+
+## estimate emissions for waterbodies in the mrb
+data %<>%
+  mutate(co2.emissions = area_ha_new * co2.tonnes.ha.y.2021,
+         ch4.emissions = area_ha_new * (ch4.tonnes.ha.y.2021 + (ch4.tonnes.ha.y.2021 * 0.09)),
+         co2.reduction = co2.emissions * incr.co2.mean.pct,
+         ch4.reduction = ch4.emissions * incr.ch4.mean.pct)
+
+## get total emissions reductions
+data %>%
+  st_drop_geometry %>%
+  summarize(co2.reduction = sum(co2.reduction, na.rm = T),
+            ch4.reduction = sum(ch4.reduction, na.rm = T))
+
+## count number of waterbodies with co2 emissions estimates
+data %>%
+  st_drop_geometry %>%
+  filter(!is.na(co2.reduction)) %>%
+  nrow
+
+## export results by state
+for(STATE in mrb.states){
+  data %>%
+    filter(stusps == STATE) %>%
+    st_write(paste0('output/mrb_emissions_reductions/mrb_emissions_reductions_', STATE, '.shp'))
+}
+
+## export results by state as text files
+for(STATE in mrb.states){
+  data %>%
+    st_drop_geometry %>%
+    filter(stusps == STATE) %>%
+    write_parquet(paste0('output/mrb_emissions_reductions_text/mrb_emissions_reductions_text_globalid_', STATE, '.parquet'))
+}
+
+## read results by state as text files (from above)
+## function to read all files in a directory
+read_all = 
+  function(x) {
+    read_parquet(x)
+  }
+
+data = 
+  list.files('output/mrb_emissions_reductions_text', pattern = "*.parquet", full.names = T) %>%
+  map_df(~read_all(.))
+
+## get results 
+data %>% 
+  select(ch4.reduction, co2.reduction) %>% 
+  summarise_all(sum, na.rm = T)
+
+##########################
+###################### n2o
+##########################
+
+## get n2o data from Alisa.Keyser@colostate.edu and Stephen.Ogle@colostate.edu in an email on 3/3/2023
+n2o.mrb =
+  right_join(
+    read_csv('store/n2o_in_mrb/MSRBgrid_NO3_wIPCC_2023-03-03.csv')[1:7],
+    read_csv('store/climate_zones_for_mrb_n2o_gridcells.csv'), 
+    by = 'cellID_10km') 
+
+## get average emissions rates by subtype and climate to identify closest subtypes and climates
+n2o.mrb %>% 
+  group_by(climate.zone) %>% 
+  summarize(`Nitrogen_Leach_Rate_gN/m2/yr` = mean(`Nitrogen_Leach_Rate_gN/m2/yr`, na.rm = T)) %>% 
+  arrange(climate.zone, `Nitrogen_Leach_Rate_gN/m2/yr`)
+
+##
+# climate.zone         `Nitrogen_Leach_Rate_gN/m2/yr`
+# <chr>                                         <dbl>
+#   1 Boreal                                       0.0768
+# 2 Cool Temperate                               1.05  
+# 3 Tropical Dry/Montane                         0.0103
+# 4 Tropical Moist/Wet                           3.35  
+# 5 Warm Temperate Dry                           0.185 
+# 6 Warm Temperate Moist                         2.74  
+
+## check climate zones
+table(n2o.mrb$climate.zone)
+table(emissions.by.type$climate)
+
+## get comparable climate zones for merging emissions reductions
+n2o.mrb %<>%
+  mutate(climate.ches.bay = case_when(climate.zone == 'Boreal' ~ 'Cool Temperate',
+                                      T ~ 'Warm Temperate Moist'))
+
+## merge with chesapeake bay emissions reductions
+n2o.mrb %<>% 
+  left_join(emissions.by.type %>% 
+              select(-subtype), 
+            by = c('climate.ches.bay' = 'climate'))
+
+## get results
+n2o.mrb %>% 
+  summarize(
+    n2o.reductions = 
+      sum(                                        ## summing across all grid cells in the mrb
+        `Nitrogen_Leach_Rate_gN/m2/yr` *          ## leach rate in grams of N per meter squared 
+          0.0075 *                                ## this converts leached N to N2O-N (IPCC rate), not entirely sure what this means but jake does because he's smart
+          ModeledPct_total_CropGrass_area/100 *   ## gives percentage of each grid cell that is responsible for leaching
+          1e4 * 1e4 *                             ## this is the area of each grid cell in meters
+          1e-6 *                                  ## grams to tonnes
+          44/14 *                                 ## this converts N to N2O (or, if you're paying attention, N2O-N to N2O)
+          incr.n2o.mean.pct,                      ## this is the climate zone specific reduction from the policy (as modeled in the CB lakes model)
+        na.rm = T
+      )
+  ) 
+
+## get results 
+n2o.mrb %>% 
+  summarize(
+    sum(                                        ## summing across all grid cells in the mrb
+      `Nitrogen_Leach_Rate_gN/m2/yr` *          ## leach rate in grams of N per meter squared 
+        0.0075 *                                ## this converts leached N to N2O-N (IPCC rate), not entirely sure what this means but jake does because he's smart
+        ModeledPct_total_CropGrass_area/100 *   ## gives percentage of each grid cell that is responsible for leaching
+        1e4 * 1e4 *                             ## this is the area of each grid cell in meters
+        1e-6 *                                  ## grams to tonnes
+        44/14,                                  ## this converts N to N2O (or, if you're paying attention, N2O-N to N2O)
+      na.rm = T
+    )
+  ) 
+
+##########################
+##################### plot
+##########################
+
+ggplot() +
+  geom_sf(data  = us_states,
+          color = 'black',
+          alpha = 0.05) + 
+  geom_sf(data  = watershed,
+          color = "#56B4E9",
+          fill  = "#56B4E9",
+          alpha = 0.1) +
+  geom_sf(data  = data,
+          aes(color = climate.zone,
+              fill  = climate.zone)) +
+  scale_color_manual(values = colors) +
+  scale_fill_manual(values = colors) +
+  labs(color = '',
+       fill  = '',
+       label = '') +
+  theme_void() +
+  theme(legend.position = 'bottom') + 
+  guides(color = guide_legend(nrow = 3),
+         fill  = guide_legend(nrow = 3))
+
+## end of script. have a great day! 
